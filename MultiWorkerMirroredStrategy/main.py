2c4075154aca8bf9744bf742b172e9afc117b1a4
Swaminathan Sriram <thedarkknight@Swaminathans-MacBook-Pro.local>
2022-11-13 22:18:00 -0700

    Initial Checkin - Distributed Training with TF

---
+++ MultiWorkerMirroredStrategy/main.py
@@ -0,0 +1,23 @@
+import json
+import os
+import tensorflow as tf
+import mnist_setup
+import socket
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+per_worker_batch_size = 64
+
+print(socket.gethostname())
+
+tf_config = json.loads(os.environ['TF_CONFIG'])
+num_workers = len(tf_config['cluster']['worker'])
+strategy = tf.distribute.MultiWorkerMirroredStrategy()
+
+global_batch_size = per_worker_batch_size * num_workers
+multi_worker_dataset = mnist_setup.mnist_dataset(global_batch_size)
+
+with strategy.scope():
+    # Model building/compiling need to be within `strategy.scope()`.
+    multi_worker_model = mnist_setup.build_and_compile_cnn_model()
+
+multi_worker_model.fit(multi_worker_dataset, epochs=3, steps_per_epoch=70)
